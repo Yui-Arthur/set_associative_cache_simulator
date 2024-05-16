42ccec95f62561860cd8cde918f282bd444449f8
Yui-Arthur <arthur80633@gmail.com>
2024-05-17 01:04:47 +0800

    update inputfile folder and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## Usage
+```bash
+gcc main.c -o main.out
+
+./main.out input.txt {byte_cache_size} {word_block_size} {set_degree}
+./main.out {input_file} 128 128 128
+```
--- main.c
+++ main.c
@@ -4,21 +4,24 @@ int main(int argc, char **argv){
     
     
     struct simulator *s = malloc(sizeof(struct simulator));
-    // read_argc_and_init(&s, argc, argv);
-    int input[3] = {1,2,3};
-    for(int i = 128; i <= 1024; i*=2){
-        input[0] = i; 
-        for(int j = 8; j <= 64; j*=2){
-            input[1] = j;
-            for(int k=2; k<=8; k*=2){
-                printf("%d, %d, %d\n", i, j, k);
-                input[2] = k;
-                init_simulator(s, "input.txt", input);
-                start_simulation(s);
-                printf("\n");
-            }
-        }
-    }
+    read_argc_and_init(s, argc, argv);
+    start_simulation(s);
+    
+    // init_simulator(s, "input.txt", input);
+    // int input[3] = {1,2,3};
+    // for(int i = 128; i <= 1024; i*=2){
+    //     input[0] = i; 
+    //     for(int j = 8; j <= 64; j*=2){
+    //         input[1] = j;
+    //         for(int k=2; k<=8; k*=2){
+    //             printf("%d, %d, %d\n", i, j, k);
+    //             input[2] = k;
+    //             init_simulator(s, "input.txt", input);
+    //             start_simulation(s);
+    //             printf("\n");
+    //         }
+    //     }
+    // }
     
 
     
